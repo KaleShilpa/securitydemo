40e1652263584be21586aa712d8ea5a5abfdc943
Shilpa Kale <shilpa_i_kale@yahoo.com>
2025-02-13 15:20:53 +0100

    added inmemory authentication

--- src/main/java/com/example/securitydemo/SecurityConfig.java
+++ src/main/java/com/example/securitydemo/SecurityConfig.java
@@ -6,6 +6,10 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AuthorizeHttpRequestsConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -15,8 +19,15 @@ public class SecurityConfig {
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests((requests) -> ((AuthorizeHttpRequestsConfigurer.AuthorizedUrl)requests.anyRequest()).authenticated());
-        //http.formLogin(Customizer.withDefaults());
+       // http.formLogin(Customizer.withDefaults());
         http.httpBasic(Customizer.withDefaults());
         return (SecurityFilterChain)http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService(){
+        UserDetails admin = User.withUsername("admin").password("{noop}admin").roles("ADMIN").build();
+        UserDetails user = User.withUsername("user").password("{noop}user").roles("USER").build();;
+        return new InMemoryUserDetailsManager(admin,user);
     }
 }
